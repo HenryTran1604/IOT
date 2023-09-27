0e7ad87b7b55aaa56da1adc90f4f79f55a042fbf
henrytran1604 <quanghuy1604nd@gmail.com>
2023-09-27 14:58:14 +0700

    27/09/2023

--- GUI.py
+++ GUI.py
@@ -13,6 +13,7 @@ class GUI(Tk):
         self.prev_frame_time = 0
         self.path = 0
         self.cap = cv2.VideoCapture(self.path)
+        self.is_vehicle = False
         self.auto = True
         self.model = Model()
         self.init_frame_function()
@@ -24,12 +25,13 @@ class GUI(Tk):
         self.frm_entrance = Frame(self.frm_function, borderwidth=1, relief='solid')
         self.frm_exit = Frame(self.frm_function, borderwidth=1, relief='solid')
         self.canvas = Canvas(self.frm_camera, width=640, height=480)
-        self.lbl_license_img = Label(self.frm_function, text='Check')
-        self.lbl_license_info = Label(self.frm_function, text='Biển số xe', font=self.font)
-        self.lbl_mode = Label(self.frm_function, text='Chế độ: Auto', font=self.font)
+        self.lbl_license_plate_img = Label(self.frm_function, text='Check')
+        self.lbl_license_plate = Label(self.frm_function, text='Biển số xe', font=self.font)
+        self.lbl_license_plate_text = Label(self.frm_function, text='')
+        self.lbl_mode = Label(self.frm_function, text='Chế độ: Auto', font=self.font, anchor='e')
         self.lbl_entrance = Label(self.frm_function, text='Cửa vào', font=self.font)
         self.lbl_exit = Label(self.frm_function, text='Cửa ra', font=self.font)
-        self.btn_change_mode = Button(self.frm_function, text='Đổi')
+        self.btn_change_mode = Button(self.frm_function, text='Đổi', command=self.change_mode)
         self.btn_entrance_open = Button(self.frm_entrance, text='MỞ', width=10)
         self.btn_entrance_close = Button(self.frm_entrance, text='ĐÓNG', width=10)
         self.btn_exit_open = Button(self.frm_exit, text='MỞ', width=10)
@@ -41,39 +43,28 @@ class GUI(Tk):
         self.frm_entrance.grid_propagate(False)
         self.frm_exit.grid_propagate(False)
         self.frm_function.config(width=308, height=480)
-        self.frm_entrance.config(width=150, height=200)
-        self.frm_exit.config(width=150, height=200)
-
-    def change_to_auto(self):
-        for child in self.frm_entrance.winfo_children():
-            child.configure(state='disable')
-        for child in self.frm_exit.winfo_children():
-            child.configure(state='disable')
-
-    def change_to_human(self):
-        for child in self.frm_entrance.winfo_children():
-            child.configure(state='normal')
-        for child in self.frm_exit.winfo_children():
-            child.configure(state='normal')
+        self.frm_entrance.config(width=150, height=180)
+        self.frm_exit.config(width=150, height=180)
 
     def align_components(self):
         self.frm_camera.pack(side='left')
         self.frm_function.pack(pady=10)
 
         self.canvas.grid(row=0, column=0, columnspan=6, rowspan=8)
-        self.lbl_license_img.grid(row=0, column=6, columnspan=2, rowspan=2)
-        self.lbl_license_info.grid(row=2, column=6, columnspan=2, sticky='nsew')
-        self.lbl_mode.grid(row=3, column=6, columnspan=2, pady=20)
-        self.lbl_entrance.grid(row=4, column=6, pady=10)
-        self.lbl_exit.grid(row=4, column=7)
-        self.frm_entrance.grid(row=5, column=6)
-        self.frm_exit.grid(row=5, column=7)
-        self.btn_change_mode.grid(row=3, column=7, padx=5)
+        self.lbl_license_plate_img.grid(row=0, column=6, columnspan=2, rowspan=2)
+        self.lbl_license_plate.grid(row=2, column=6, columnspan=2)
+        self.lbl_license_plate_text.grid(row=3, column=6, columnspan=2)
+        self.lbl_mode.grid(row=4, column=6, pady=20)
+        self.lbl_entrance.grid(row=5, column=6, pady=10)
+        self.lbl_exit.grid(row=5, column=7)
+        self.frm_entrance.grid(row=6, column=6)
+        self.frm_exit.grid(row=6, column=7)
+        self.btn_change_mode.grid(row=4, column=7, padx=50)
         self.btn_entrance_open.grid(row=0, column=0, padx=40, pady=30, columnspan=2)
         self.btn_entrance_close.grid(row=1, column=0, padx=40, pady=50, columnspan=2)
         self.btn_exit_open.grid(row=0, column=0, padx=40, pady=30, columnspan=2)
         self.btn_exit_close.grid(row=1, column=0, padx=40, pady=50, columnspan=2)
-        self.btn_end_program.grid(row=6, column=7, sticky='e')
+        self.btn_end_program.grid(row=7, column=7, sticky='e')
 
     @staticmethod
     def convert_image(img):
@@ -88,7 +79,9 @@ class GUI(Tk):
             fps = 1 / (self.curr_frame_time - self.prev_frame_time)
             fps = int(fps)
             self.prev_frame_time = self.curr_frame_time
-            frame = self.model.detect(ret, frame)
+            list_license_plates = set()
+            if self.is_vehicle:
+                frame, list_license_plates = self.model.detect(ret, frame)
             photo = GUI.convert_image(frame)
 
             self.canvas.create_image(0, 0, image=photo, anchor=NW)
@@ -101,15 +94,42 @@ class GUI(Tk):
                 crop_image = cv2.imread('nf.png')
             crop_image = cv2.resize(crop_image, (300, 100))
             crop_image_photo = GUI.convert_image(crop_image)
-            self.lbl_license_img.config(image=crop_image_photo)  # Cập nhật hình ảnh trên Label
-            self.lbl_license_img.image = crop_image_photo
+            self.lbl_license_plate_img.config(image=crop_image_photo)  # Cập nhật hình ảnh trên Label
+            self.lbl_license_plate_img.image = crop_image_photo
+            self.lbl_license_plate_text.config(text= ', '.join(list_license_plates))
 
-            self.canvas.create_text(50, 50, text=str(fps), font=("Arial", 20), fill="red")
+            self.canvas.create_text(50, 50, text=str(fps), font=self.font, fill="green")
             self.after(15, self.update_frame_camera)  
 
     def run(self):
         self.update_frame_camera()
         self.mainloop()
+
+
+    # -----------------------------function----------------------------
+    def change_to_auto(self):
+        self.is_vehicle = True
+        self.lbl_mode.config(text='Chế độ: Auto')
+        for child in self.frm_entrance.winfo_children():
+            child.configure(state='disable')
+        for child in self.frm_exit.winfo_children():
+            child.configure(state='disable')
+
+    def change_to_human(self):
+        self.is_vehicle = False
+        self.lbl_mode.config(text='Chế độ: Human')
+        for child in self.frm_entrance.winfo_children():
+            child.configure(state='normal')
+        for child in self.frm_exit.winfo_children():
+            child.configure(state='normal')
+
+    def change_mode(self):
+        self.auto = not self.auto
+        if self.auto:
+            self.change_to_auto()
+        else:
+            self.change_to_human()
+
 if __name__ == '__main__':
     a = GUI()
     a.run()
--- webcam.py
+++ webcam.py
@@ -27,21 +27,19 @@ class Model:
             h = int(plate[3] - plate[1]) # ymax - ymin  
             crop_img = frame[y:y+h, x:x+w]
             cv2.rectangle(frame, (int(plate[0]),int(plate[1])), (int(plate[2]),int(plate[3])), color = (0,0,225), thickness = 2)
-            cv2.imwrite("crop.jpg", crop_img)
-            rc_image = cv2.imread("crop.jpg")
             lp = ""
             for cc in range(0,2):
                 for ct in range(0,2):
                     lp = helper.read_plate(self.yolo_license_plate, utils_rotate.deskew(crop_img, cc, ct))
                     if lp != "unknown":
                         list_read_plates.add(lp)
-                        print(lp)
                         cv2.putText(frame, lp, (int(plate[0]), int(plate[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+                        cv2.imwrite("crop.jpg", crop_img)
                         flag = 1
                         break
                 if flag == 1:
                     break
-        return frame
+        return frame, list_read_plates
 # prev_frame_time = 0
 # new_frame_time = 0
 # path = 'http://192.168.0.100:4747/video'
